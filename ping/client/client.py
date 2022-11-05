727fb5fbb5d7eaeb88a2973333ea17def0026971
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2022-11-05 13:05:46 -0300

    ✨ feat: Improve client package generation

--- ping/client/client.py
+++ ping/client/client.py
@@ -1,14 +1,40 @@
 import socket
+import time
+import random
+import string
+
+
+def get_timestamp() -> int:
+    '''Get current timestamp
+    On Windows and most Unix systems, the timestamp is the last four
+    time numbers in seconds since the epoch (Unix time).
+    :param None
+    : return timestamp - int, last four digits of unix time
+    '''
+    return round(time.time()) % 10000
 
 
 def create_message(package_id: int, message: str) -> bytes:
-    '''.'''
-    id_str = str(package_id).ljust(5, '0')
+    '''Generate a standard-compliant package
+    In order, the package consists of:
+        - 5 bytes, sequence number
+        - 1 byte, ping = 0 or pong = 1
+        - 4 bytes, timestamp
+        - 30 bytes, package content
+    :param package_id - int, sequence number
+    :param message - str, package content
+    :return package - bytes, package as byte stream
+    '''
+    id_str = str(package_id).rjust(5, '0')
     ping_std = '0'  # 0 = ping, 1 = pong
-    timestamp = '0000'  # initial timestamp is zero
-    package_message = id_str + ping_std + timestamp + message.rjust(30, '\0')
+    timestamp = str(get_timestamp())  # initial timestamp
+    package_message = id_str + ping_std + timestamp + message.ljust(30, '\0')
     return package_message.encode('utf8')
 
 
+# generate message
+content_size = random.randint(1, 30)
+CONTENT = ''.join(random.choices(string.ascii_lowercase, k=content_size))
+
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp.sendto(create_message(0, 'batata'), ('127.0.0.1', 3000))
+udp.sendto(create_message(0, CONTENT), ('127.0.0.1', 3000))
