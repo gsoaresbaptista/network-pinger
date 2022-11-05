8c94353b20ce4615189fa262b16fec3a8476d6ba
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2022-11-05 15:16:57 -0300

    ✨ feat: Make the client receive response

--- ping/client/client.py
+++ ping/client/client.py
@@ -5,12 +5,18 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
-from ping.package import create_package
-
+from package import create_package, read_package
 
 # generate message
 content_size = random.randint(1, 30)
 CONTENT = ''.join(random.choices(string.ascii_lowercase, k=content_size))
 
+# send message
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp.sendto(create_package(0, 0, CONTENT), ('127.0.0.1', 3000))
+
+# receive message
+udp.settimeout(3)
+response, _ = udp.recvfrom(40)
+print(read_package(response.decode('ascii')))
+# TODO: Check data
