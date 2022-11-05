15ab0fc890a7cfc810d854987e17783dc97933dd
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2022-11-05 16:55:02 -0300

    ♻️ refactor: Remove package check function from client code

--- ping/client/abstract_client.py
+++ ping/client/abstract_client.py
@@ -58,28 +58,3 @@ class AbstractClient(ABC):
         '''
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(f"{now} - {category:5} | {message}")
-
-    @staticmethod
-    def _check_package_state(
-        sid: str, ptype: str, time: str, content: str
-    ) -> bool:
-        '''Check package consistency
-        :param sid - str, sequence number
-        :param ptype - char, 0 to ping and 1 to pong
-        :param time - str, timestamp
-        :param content - str, package content
-        :return True if it passes all integrity conditions, False otherwise
-        '''
-        if not sid.isnumeric():
-            AbstractClient.emmit('ERROR', 'Non numeric sequence number')
-            return False
-        if ptype != '0':
-            AbstractClient.emmit('ERROR', 'Received pong instead of ping')
-            return False
-        if not time.isnumeric():
-            AbstractClient.emmit('ERROR', 'Non numeric timestamp')
-            return False
-        if len(content) > 30:
-            AbstractClient.emmit('ERROR', 'Larger content than allowed')
-            return False
-        return True
