f56c7b4ddd4423cdc4d6bbeb2c0869ac541d4203
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2022-11-05 13:12:15 -0300

    🎨 improve: Improve server architecture

--- ping/server/abstract_server.py
+++ ping/server/abstract_server.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Union, Tuple
+from typing import Dict, Tuple
 import os
+import sys
 import datetime
 import socket
 
@@ -8,7 +9,7 @@ import socket
 class AbstractServer(ABC):
     '''Assign Interface Contracts to a server object.'''
 
-    def __init__(self, timeout: Union[float, int] = 5) -> None:
+    def __init__(self, timeout: float | int = 5) -> None:
         super().__init__()
         # set initial environment
         os.environ["TZ"] = "UTC"
@@ -32,18 +33,59 @@ class AbstractServer(ABC):
         '''
 
     @abstractmethod
-    def check(self) -> Dict[str, Union[int, float, str]]:
+    def check(self) -> Dict[str, int | float | str]:
         '''Return server state.
         :param None
         :return None
         '''
 
-    @abstractmethod
     def listen(self) -> None:
-        '''Makes the server listen and expect to receive some data.'''
+        '''Makes the server listen and expect to receive some data.
+        :param None
+        :return None
+        '''
+        running: bool = True
 
-    def emmit(self, message: str) -> None:
+        try:
+            while running:
+                self._listen_one()
+                sys.stdout.flush()
+        except KeyboardInterrupt:
+            self.disconnect()
+
+    def emmit(self, category: str, message: str) -> None:
         '''Emmit a message to standart output.
-        :param message - str, text to emmit'''
-        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-        print(f"{now} - {type(self).__name__} | {message}")
+        :param message - str, text to emmit
+        :return None
+        '''
+        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"{now} - {category:5} | {message}")
+
+    # private methods, using defined pattern for packages
+    def _handle_package(self, package: str) -> Tuple[int, int, int, str]:
+        '''Handle a standard-compliant package
+        Unpack all package data into single variables
+        :param package - str, packet decoded in utf8 format
+        :return
+            - id_sequence - int, sequence number
+            - package_type - int, 0 if ping or 1 for pong
+            - timestamp - int, timestamp in seconds
+            - content - str, package content
+        '''
+        id_sequence = int(package[:5])
+        package_type = int(package[5])
+        timestamp = int(package[6:10])
+        content = package[10:]
+
+        return id_sequence, package_type, timestamp, content
+
+    def _listen_one(self) -> None:
+        '''Procedure to handle a packaged in the defined pattern.
+        :param None
+        :return None
+        '''
+        package, received_address = self._connection.recvfrom(64)
+        address = f"{received_address[0]}:{received_address[1]}"
+        message = package.decode('utf8')
+        seq, req, timestamp, content = self._handle_package(package)
+        self.emmit('RECV', f"'{content}' received from {address}")
--- ping/server/udp_server.py
+++ ping/server/udp_server.py
@@ -1,14 +1,14 @@
 import socket
-from typing import Dict, Union
+from typing import Dict
 from server.abstract_server import AbstractServer
 
 
 class UDPServer(AbstractServer):
     '''UDP server implementation'''
 
-    def __init__(self, timeout: Union[float, int] = 5) -> None:
+    def __init__(self, timeout: float | int = 5) -> None:
         super().__init__(timeout)
-        self.emmit('UDP Server initialized')
+        self.emmit('INIT', 'UDP Server initialized')
 
     def connect(self, server_ip: str, server_port: int) -> None:
         '''Hosts server on server_ip on server_port.
@@ -20,7 +20,7 @@ class UDPServer(AbstractServer):
         self._connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._connection.settimeout(self._timeout)
         self._connection.bind(self._address)
-        self.emmit(f"Running on {server_ip}:{server_port}")
+        self.emmit('INIT', f"Running on {server_ip}:{server_port}")
 
     def disconnect(self) -> None:
         '''Close server connection.
@@ -28,9 +28,9 @@ class UDPServer(AbstractServer):
         :return None
         '''
         self._connection.close()
-        self.emmit('Server connection closed')
+        self.emmit('END', 'Server connection closed')
 
-    def check(self) -> Dict[str, Union[int, float, str]]:
+    def check(self) -> Dict[str, int | float | str]:
         '''Return server state.
         :param None
         :return None
@@ -40,9 +40,3 @@ class UDPServer(AbstractServer):
             'server_port': self._address[1],
             'timeout_time': self._timeout,
         }
-
-    def listen(self) -> None:
-        '''.'''
-        message, address = self._connection.recvfrom(40)
-        address = f"{address[0]}:{address[1]}"
-        self.emmit(f"'{message.decode('utf8')}' received from {address}")
