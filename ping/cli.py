337a8825fd7ff5d96e421d2c25310c877dff8849
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2022-11-05 12:50:00 -0300

    ✨ feat: Add log file

--- ping/cli.py
+++ ping/cli.py
@@ -1,6 +1,37 @@
+import sys
+import argparse
 from server import Server, UDPServer
 
-server: Server = UDPServer()
-server.connect('127.0.0.1', 3000)
-server.listen()
-server.disconnect()
+
+if __name__:
+    # arguments
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [OPTION]...",
+        description=(
+            "Run a server to receive ping and respond "
+            "with pong to compute rtt."
+        ),
+    )
+    parser.add_argument(
+        "-l",
+        "--logger",
+        help='Save all server output in log file.',
+        action='store_true',
+    )
+    args = parser.parse_args()
+
+    # stdout change
+    if args.logger:
+        previous = sys.stdout
+        sys.stdout = open('log.txt', 'a', encoding='utf8')
+
+    # server run
+    server: Server = UDPServer()
+    server.connect('127.0.0.1', 3000)
+    server.listen()
+    server.disconnect()
+
+    # revert stdout change
+    if args.logger:
+        sys.stdout.close()
+        sys.stdout = previous
